409bd9076d6e24b5eae3534d43bb3248d0c61c58
wolterbwambale <benplaan1@gmail.com>
2023-08-11 14:17:37 +0300

    update the file

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -34,4 +34,19 @@ describe Solver do
       expect(solver.reverse('world')).to eq('dlrow')
     end
   end
+
+  describe '#fizzbuzz' do
+    it "returns 'fizz' for input 3" do
+      expect(solver.fizzbuzz(3)).to eq('fizz')
+    end
+    it "returns 'buzz' for input 5" do
+      expect(solver.fizzbuzz(5)).to eq('buzz')
+    end
+    it "returns 'fizzbuzz' for input 15" do
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+    it "returns '7' for input 7" do
+      expect(solver.fizzbuzz(7)).to eq('7')
+    end
+  end
 end
